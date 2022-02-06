56b588e98842970256536eb106aea050d6c435eb
nigmasilmi <sn.silmi@gmail.com>
2022-02-06 12:26:32 -0300

    step 14 in dev-notes

--- src/components/hooks/use-http.js
+++ src/components/hooks/use-http.js
@@ -1,29 +1,32 @@
 import { useState, useCallback } from 'react';
-const useHttp = (requestConfig, applyData) => {
+const useHttp = (applyData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(requestConfig.url, {
-        method: requestConfig.method ? requestConfig.method : 'GET',
-        headers: requestConfig.headers ? requestConfig : {},
-        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-      });
+  const sendRequest = useCallback(
+    async (requestConfig) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          headers: requestConfig.headers ? requestConfig : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+        // limit the response to json data
+        const data = await response.json();
+        applyData(data);
+      } catch (err) {
+        setError(err.message || 'Something went wrong!');
       }
-      // limit the response to json data
-      const data = await response.json();
-      applyData(data);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  }, [requestConfig, applyData]);
+      setIsLoading(false);
+    },
+    [applyData]
+  );
   return { error, isLoading, sendRequest };
 };
 
